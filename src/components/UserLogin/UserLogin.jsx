0dab4cd9dc7a23ffa5a640d8528fd87c7e19e4b7
Stefi Sosa <stefisosa65@gmail.com>
2023-03-09 20:47:14 -0300

    pagina de login, componentes LoginSocialNetwork y UserLogin todos con su archivo css. Instale reactIcons y reactHookForm

---
+++ src/components/UserLogin/UserLogin.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "../UserLogin/UserLogin.css";
+import { useForm } from "react-hook-form";
+
+function UserLogin() {
+  const onSubmit = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="inputUser"
+          type="email"
+          id="email"
+          pattern=".+@globex\.com"
+          size="15"
+          placeholder="Usuario"
+          required
+          {...register("email", { required: true, minLength: 8 })}
+        />
+        {errors.email && <p>El email es requerido</p>}
+        <input
+          className="inputPassword"
+          type="password"
+          name="pass"
+          minLength="8"
+          maxLength="20"
+          placeholder="Contraseña"
+          required
+          {...register("password", { required: true, minLength: 8 })}
+        />
+        {errors.password && (
+          <p>La contraseña debe tener un minimo de 8 caracteres</p>
+        )}
+
+        <button className="buttonLogin" type="submit">
+          Iniciar Sesión
+        </button>
+        <button className="buttonRegister" type="submit">
+          Registrarse
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default UserLogin;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Login from "./pages/login/Login";
 //import "./index.css";
 import Welcome from "./pages/welcome/Welcome";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Welcome />
+    {/* <Login /> */}
   </React.StrictMode>
 );
