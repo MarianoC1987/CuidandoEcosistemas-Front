8e6f9c5093cc4ff430ebc9b262fb0f94e48401c4
David Escandón <david.escandon.v@gmail.com>
2023-03-14 11:18:02 -0300

    push rama de david

--- src/Components/ButtonsLoginRegister/LoginRegister.jsx
+++ src/Components/ButtonsLoginRegister/LoginRegister.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useForm }  from "react-hook-form";
+import arrowLeft from "./img/arrow_back_ios_new.svg";
+import { Link } from "react-router-dom";
+
 import "./loginRegister.css";
 
 function LoginRegister() {
@@ -13,22 +17,48 @@ function LoginRegister() {
   //   width: "100%",
   //   height: "100%"
   // };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <div className='divStyles'>
+      <header>
+        <div className="header1">
+        <Link to={`/welcome`}>
 
+            <img className="arrowLeft" src={arrowLeft} alt="Flecha" />
+        </Link>
+
+         </div>
+      </header>
       <form  className='formStyles'>
           <label className='inputLabel' id='inputLabel1'>
-            <input class="inputText" type="text" name="name"  />
+            <input 
+              class="inputText" 
+              type="text" 
+              name="name"  
+              {...register('name', { required: true })}
+            />
+            {errors.name && <p>Last name is required.</p>}
           </label>
           <label className='inputLabel' id='inputLabel2'>
-            <input class="inputText" type="text" name="" />
+            <input 
+              class="inputText" 
+              type="mail" 
+              name="" 
+              {...register('mail', { required: true })}
+            />
           </label>
           <label className='inputLabel'  id='inputLabel3'>
             <input class="inputText" type="password" name="password" />
           </label>
           <input id="buton1" type="submit" value="Inciar Sesión" />    
-          <input id="buton2" type="submit" value="Register" />    
+          <button id="buton2" type="submit">
+          Registrarse
+          </button>    
 
         </form>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
 //import "./index.css";
 import Welcome from "./pages/welcome/Welcome";
 import UserRegister from "./pages/UserRegister/UserRegister";
 
+const router = createBrowserRouter([
+  {
+    path:"/welcome",
+    element: <Welcome/>,
+  },
+  {
+    path:"/welcome/register",
+    element: <UserRegister/>,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Welcome /> */}
-    <UserRegister/>
-
+    {/* <UserRegister/> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
