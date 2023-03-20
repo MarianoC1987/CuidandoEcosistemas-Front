9d4f86149ed2bdc4f25897ef23e3c8de343cf35c
Mariano Camacho <macadema@gmail.com>
2023-03-20 18:11:32 -0300

    search bar, account icon y boxes en Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,18 +2,35 @@ import "./home.css";
 import logo from "../../assets/images/logo-sinletras.png";
 import MenuBar from "../../components/MenuBar/MenuBar";
 import Borders from "../../components/Borders/Borders";
+import AccountBox from "../../assets/images/account_box.png";
+import Search from "../../assets/images/Search.png";
 
 function Home() {
   return (
-    <main className="home-main">
-      <Borders className="home-borders" />
-      <div className="home-logo">
+    <main className="homeMain">
+      <Borders className="homeBorders" />
+      <div className="homeLogo">
         <img src={logo} />
       </div>
-      {/* <section>usuario</section>
-      <section>search-bar</section>
-      <section>3 cuadrados</section>
-      <section></section> */}
+      <div className="homeSections">
+        {/* <section className="home-sectionOne">usuario</section> */}
+        <img className="homeSectionOne" src={AccountBox} />
+        <section className="homeSectionTwo">
+          <input
+            className="homeSearch"
+            type="text"
+            placeholder="Buscar"
+            name="search"
+          ></input>
+          <img src={Search} alt="search icon" />
+        </section>
+        <section className="homeSectionThree">
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <section></section>
+      </div>
       <MenuBar />
     </main>
   );
