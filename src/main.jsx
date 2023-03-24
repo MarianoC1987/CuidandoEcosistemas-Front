041cbf471c37f2b8cce311b460ca3038330bff45
Mariano Camacho <macadema@gmail.com>
2023-03-24 19:48:18 -0300

    componente my plants y localstorage en componente home

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import Home from "./pages/home/Home";
 import Welcome from "./pages/welcome/Welcome";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Cubism from "./components/CubismBackground/Cubism";
 import Login from "./pages/login/login";
 import Register from "./pages/register/Register";
+import MyPlants from "./pages/myplants/MyPlants";
 
 const router = createBrowserRouter([
   { name: "Welcome", path: "/", element: <Welcome /> },
   { name: "Home", path: "home", element: <Home /> },
   { name: "Login", path: "login", element: <Login /> },
   { name: "Register", path: "register", element: <Register /> },
-  { name: "MyPlants", path: "myplants", element: <Cubism /> },
+  { name: "MyPlants", path: "myplants", element: <MyPlants /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
